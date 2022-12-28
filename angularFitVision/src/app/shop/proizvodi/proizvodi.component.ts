a8482ba9acad3f68b5d6df1347a61182d6e06797
adil <adil@example.com>
2022-12-28 11:10:45 +0100

    azuriranje proizvoda

--- angularFitVision/src/app/shop/proizvodi/proizvodi.component.ts
+++ angularFitVision/src/app/shop/proizvodi/proizvodi.component.ts
@@ -14,9 +14,10 @@ declare function porukaError(a: string):any;
 })
 export class ProizvodiComponent implements OnInit {
 
-  constructor(private httpKlijent:HttpClient) { }
+  constructor(private httpKlijent: HttpClient) {
+  }
 
-  loginInfo():LoginInformacije {
+  loginInfo(): LoginInformacije {
     return AutentifikacijaHelper.getLoginInfo();
   }
 
@@ -26,55 +27,58 @@ export class ProizvodiComponent implements OnInit {
     this.ucitajProizvode();
   }
 
-  ucitajProizvode(){
-    this.httpKlijent.get(MojConfig.adresa_servera + "/Proizvod/GetAll").subscribe(x=>{
-      this.proivodi=x;
+  ucitajProizvode() {
+    this.httpKlijent.get(MojConfig.adresa_servera + "/Proizvod/GetAll").subscribe(x => {
+      this.proivodi = x;
     })
   }
 
-  ucitajBrendove(){
-    this.httpKlijent.get(MojConfig.adresa_servera + "/Brend/GetAll").subscribe(x=>{
-      this.brendovi=x;
+  ucitajBrendove() {
+    this.httpKlijent.get(MojConfig.adresa_servera + "/Brend/GetAll").subscribe(x => {
+      this.brendovi = x;
     })
   }
 
-  ucitajPodKategorije(){
-    this.httpKlijent.get(MojConfig.adresa_servera + "/Podkategorija/GetAll").subscribe(x=>{
-      this.podKategorije=x;
+  ucitajPodKategorije() {
+    this.httpKlijent.get(MojConfig.adresa_servera + "/Podkategorija/GetAll").subscribe(x => {
+      this.podKategorije = x;
     })
   }
 
-  proizvodObject:any;
-  brendovi:any;
-  podKategorije:any
+  proizvodObject: any;
+  brendovi: any;
+  podKategorije: any
   proivodi: any;
-  detaljiProizvod:any;
+  detaljiProizvod: any;
+  url: "";
 
 
   dodajFunc() {
-    this.proizvodObject={
-      id:0,
-      naziv:"",
-      jedinicna_cijena:0,
-      sastav:"",
-      jedinicna_mjera:"",
-      zaliha:0,
-      slika:"assets/empty.png",
+    this.proizvodObject = {
+      id: 0,
+      naziv: "",
+      jedinicna_cijena: 0,
+      sastav: "",
+      jedinicna_mjera: "",
+      zaliha: 0,
+      slika: "assets/empty.png",
 
-      pod_kategorija_id:0,
-      brend_id:0
+      pod_kategorija_id: 0,
+      brend_id: 0
     }
   }
 
   snimiProizvod() {
-    if(this.proizvodObject.naziv!="" && this.proizvodObject.zaliha>=0 && this.proizvodObject.brend_id>0 &&
-      this.proizvodObject.pod_kategorija_id>0 && this.proizvodObject.jedinicna_mjera!="" && this.proizvodObject.sastav!="" &&
-      this.proizvodObject.jedinicna_cijena>0){
+    if (this.proizvodObject.naziv != "" && this.proizvodObject.zaliha >= 0 && this.proizvodObject.brend_id > 0 &&
+      this.proizvodObject.pod_kategorija_id > 0 && this.proizvodObject.jedinicna_mjera != "" && this.proizvodObject.sastav != "" &&
+      this.proizvodObject.jedinicna_cijena > 0) {
       this.httpKlijent.post(MojConfig.adresa_servera + "/Proizvod/Snimi", this.proizvodObject, MojConfig.http_opcije())
-        .subscribe(x=>{this.proizvodObject=null; this.ucitajProizvode();});
-      porukaSuccess("uspjesno dodan proizvod");
-    }
-    else{
+        .subscribe(x => {
+          this.proizvodObject = null;
+          this.ucitajProizvode();
+        });
+      porukaSuccess("uspjesno snimljen proizvod");
+    } else {
       alert("niste unijeli sve podatke");
     }
 
@@ -82,6 +86,21 @@ export class ProizvodiComponent implements OnInit {
   }
 
   detaljno(p: any) {
-    this.detaljiProizvod=p;
+    this.detaljiProizvod = p;
+  }
+
+  urediFunc(p: any) {
+    this.proizvodObject = p;
+  }
+
+  ChosenFile($event: Event) {
+    let file = (event.target as HTMLInputElement).files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e: any) => {
+      this.url = e.target.result;
+      this.proizvodObject.slika = this.url;
+   
+    }
   }
 }
